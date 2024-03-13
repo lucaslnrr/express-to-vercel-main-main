66e662fdb3d5647aff607ef6f8aea405c5e11622
lucaslnrr <lucas.lnrr@gmail.com>
2024-03-13 15:07:55 -0300

    fiscal

--- index.js
+++ index.js
@@ -55,8 +55,39 @@ app.post('/authorizePurchaseOrder', async (req, res) => {
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
+app.post('/fiscal', async (req, res) => {
+  try {
+    const fiscal = req.body.fiscal;
+    const billId = fiscal.billId;
+    const username = 'guerrero-felipesantos';
+    const password = 'd0YnARDcra45tSC3jD8ip89MMuBpm2kN'; 
+    const apiUrl = `https://api.sienge.com.br/guerrero/public/api/v1/bills/${billId}/tax-information`;
+    const response = await fetch(apiUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Basic ' + Buffer.from(`${username}:${password}`).toString('base64')
+      },
+      body: JSON.stringify({ operationTaxCode: fiscal.operationTaxCode })
+    });
 
+    console.log(fiscal);
+    console.log('Response status:', response.status);
 
+    if (response.status === 204) {
+      console.log('ok');
+      return res.status(204).end();
+    }
+
+    const responseBody = await response.json();
+    console.log('Response body:', responseBody);
+
+    res.json(responseBody); 
+  } catch (error) {
+    console.error('Error:', error.message);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
 
 app.post('/upload', upload.single('csvFile'), async (req, res) => {
     try {
